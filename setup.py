c8af0f0f5d821e2aefc1aa1a081cc8dcd7d00ce3
ccxtechnologies <charles@ccxtechnologies.com>
2017-07-26 14:11:26 -0400

    fix some build=script issues for pypi

--- setup.py
+++ setup.py
@@ -5,29 +5,32 @@
 from distutils.core import setup
 from distutils.core import Extension
 from Cython.Build import cythonize
+import sys
 
+if "--cythonize" in sys.argv:
+    sdbus = cythonize(
+        [Extension("adbus.sdbus", ["adbus/sdbus.pyx"], libraries=["systemd"])]
+    )
+else:
+    sdbus = [Extension("adbus.sdbus", ["adbus/sdbus.c"], libraries=["systemd"])]
 
 def build():
     """Build Package."""
 
-    sdbus = cythonize(
-        [Extension("adbus.sdbus", ["adbus/sdbus.pyx"], libraries=["systemd"])]
-    )
-
     setup(
         name='adbus',
-        version='0.1',
+        version='0.1.0',
         description='asyncio based dbus interface',
         license='MIT',
         author='CCX Technologies',
         author_email='charles@ccxtechnologies.com',
         url='http://github.com/ccxtechnologies/python-adbus',
+        download_url='https://github.com/ccxtechnologies/python-adbus/archive/v0.1.0.tar.gz',
         platforms=['linux'],
         provides=['adbus'],
-        packages=['adbus'],
+        packages=['adbus', 'adbus.server'],
         ext_modules=sdbus
     )
-
 
 if __name__ == "__main__":
     build()
