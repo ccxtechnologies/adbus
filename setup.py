80b0f7c8260d57d578b4279767408fc8dddfae54
Charles Eidsness <charles@ccxtechnologies.com>
2017-10-01 12:48:04 -0400

    switch from distutils to setuputils

--- setup.py
+++ setup.py
@@ -1,42 +1,54 @@
-#!/usr/bin/env python3
-# Copyright: 2017, CCX Technologies
-"""distutils build and configuration script for python-adbus."""
+#!/usr/bin/python
 
-from distutils.core import setup
-from distutils.core import Extension
 import sys
+import subprocess
 
-exec(open('adbus/__version__.py').read())
+from setuptools import setup
+from setuptools import find_packages
+from setuptools.extension import Extension
 
-if "--cythonize" in sys.argv:
-    from Cython.Build import cythonize
-    sdbus = cythonize(
-        [Extension("adbus.sdbus", ["adbus/sdbus.pyx"], libraries=["systemd"])]
-    )
-    sys.argv.remove("--cythonize")
-else:
-    sdbus = [Extension("adbus.sdbus", ["adbus/sdbus.c"], libraries=["systemd"])]
+__name__ = 'adbus'
+__url__ = 'https://github.com/ccxtechnologies'
+exec(open(f'{__name__}/__version__.py').read())
 
 
-def build():
-    """Build Package."""
+def cython(module, libraries):
+    if "--cythonize" in sys.argv:
+        # Import is here so that don't need cython if don't want
+        # to rebuild c file.
+        from Cython.Build import cythonize
+        sdbus = cythonize(
+            [Extension(f"{__name__}.{module}",
+                [f"{__name__}/{module}.pyx"], libraries=libraries)]
+        )
+        sys.argv.remove("--cythonize")
+    else:
+        sdbus = [Extension(f"{__name__}.{module}",
+            [f"{__name__}/{module}.c"], libraries=libraries)]
 
-    setup(
-        name='adbus',
-        version=__version__,
-        description='asyncio based dbus interface',
-        license='MIT',
-        author='CCX Technologies',
-        author_email='charles@ccxtechnologies.com',
-        url='http://github.com/ccxtechnologies/python-adbus',
-        download_url=
-        'https://github.com/ccxtechnologies/python-adbus/archive/v0.2.1.tar.gz',
-        platforms=['linux'],
-        provides=['adbus'],
-        packages=['adbus', 'adbus.server', 'adbus.client'],
-        ext_modules=sdbus
-    )
+def check_dependancy(name):
+    try:
+        whereis = subprocess.run(['whereis', name], check=True, stdout=subprocess.PIPE)
+        if b'/' not in whereis.stdout:
+            print(f"\033[91m Failed to find dependancy {name}.\033[0m")
+            exit(-1)
+    except subprocess.CalledProcessError:
+        print(f"\033[91m Failed to run whereis, is whereis broken?\033[0m")
+        exit(-1)
 
+check_dependancy("libsystemd")
 
-if __name__ == "__main__":
-    build()
+setup(
+    name=__name__,
+    version=__version__,
+    author='CCX Technologies',
+    author_email='charles@ccxtechnologies.com',
+    description='asyncio based dbus interface',
+    license='MIT',
+    url=f'{__url__}/{__name__}',
+    download_url=f'{__url__}/archive/v{__version__}.tar.gz',
+
+    python_requires='>=3.6',
+    packages=find_packages(),
+    ext_modules=cython('sdbus', ["systemd"])
+)
