f52a14dc767a2200668c772bc6d6a58f969a81d1
charles <charles@ccxtechnologies.com>
2019-04-15 15:37:12 -0400

    add debug info to library build, useful while library is still in development

--- setup.py
+++ setup.py
@@ -43,6 +43,8 @@ module = [Extension(
         f"{__module__}.sdbus",
         sources=[f"{__module__}/sdbus" + ext],
         libraries=["systemd"],
+        extra_compile_args=["-g"],
+        extra_link_args=["-g"],
 )]
 
 
