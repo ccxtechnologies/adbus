c5de2a33f07ce5b9e21ce4e3e0ea1cbfd082be66
charles <charles@ccxtechnologies.com>
2019-05-07 07:17:03 -0400

    clean up build process for our new builder

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python
 
-import subprocess
-import sys
-
 from setuptools import setup
 from setuptools import find_packages
 from setuptools import Extension
@@ -19,32 +16,15 @@ __url__ = 'https://github.com/ccxtechnologies'
 __version__ = None
 exec(open(f'{__module__}/__version__.py').read())
 
-# Required for CCX's Build System, do not remove
-if "--nosystemd" in sys.argv:
-    sys.argv.remove("--nosystemd")
-
-
-def check_external_dependancy(name):
-    try:
-        whereis = subprocess.run(
-                ['whereis', name], check=True, stdout=subprocess.PIPE
-        )
-        if b'/' not in whereis.stdout:
-            print(f"\033[91m Failed to find dependency {name}.\033[0m")
-    except subprocess.CalledProcessError:
-        print(f"\033[91m Failed to run whereis, is whereis broken?\033[0m")
-
-
-check_external_dependancy("libsystemd")
-
 ext = '.pyx' if USE_CYTHON else '.c'
 
-module = [Extension(
-        f"{__module__}.sdbus",
-        sources=[f"{__module__}/sdbus" + ext],
-        libraries=["systemd"],
-)]
-
+module = [
+        Extension(
+                f"{__module__}.sdbus",
+                sources=[f"{__module__}/sdbus" + ext],
+                libraries=["systemd"],
+        )
+]
 
 if USE_CYTHON:
     module = cythonize(module)
